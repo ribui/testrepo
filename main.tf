f70f5d4d097adb4346b71ddbac5716b7a74c771d
ribui <107009141+ribui@users.noreply.github.com>
2023-05-04 22:06:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 # Provision an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-0889a44b331db0194" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   key_name      = "example_key"
   
