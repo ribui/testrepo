4308829a02e948ef256174a3e73427de8b81a8eb
ribui <107009141+ribui@users.noreply.github.com>
2023-05-04 22:20:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,9 @@
 provider "aws" {
-  region = "us-east-1" # Replace with the region you want to use
+  region = "us-west-2" # Replace with the region you want to use
 }
 
-resource "aws_ecr_repository" "example" {
-  name = "my-ecr-repo" # Replace with the name you want to use
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  image_tag_mutability = "MUTABLE"
-
-  lifecycle {
-    prevent_destroy = false # Set to true to prevent accidental deletion
-  }
+resource "aws_key_pair" "example" {
+  key_name = "my-key-pair" # Replace with the name you want to use
 }
+
 
